1e86149259b6bf0810db5ac98a69dd9f820701f0
Esteban Charria Muñoz <charriaesteban9@gmail.com>
2021-12-28 18:39:07 -0500

    primer commit

---
+++ js/ventanaModular.js
@@ -0,0 +1,64 @@
+const open = document.getElementById('open');
+const modal_container = document.getElementById('modal_container');
+const close = document.getElementById('close');
+
+open.addEventListener('click',() =>{
+    modal_container.classList.add("show")
+})
+
+close.addEventListener('click',() =>{
+    modal_container.classList.remove("show")
+})
+
+const open2 = document.getElementById('open2');
+const modal_container2 = document.getElementById('modal_container2');
+const close2 = document.getElementById('close2');
+
+open2.addEventListener('click', () => {
+  modal_container2.classList.add('show2');  
+});
+
+close2.addEventListener('click', () => {
+  modal_container2.classList.remove('show2');
+});
+
+const open3 = document.getElementById('open3');
+const modal_container3 = document.getElementById('modal_container3');
+const close3 = document.getElementById('close3');
+
+open3.addEventListener('click', () => {
+  modal_container3.classList.add('show3');  
+});
+
+close3.addEventListener('click', () => {
+  modal_container3.classList.remove('show3');
+});
+
+
+const open4 = document.getElementById('open4');
+const modal_container4 = document.getElementById('modal_container4');
+const close4 = document.getElementById('close4');
+
+open4.addEventListener('click', () => {
+  modal_container4.classList.add('show4');  
+});
+
+close4.addEventListener('click', () => {
+  modal_container4.classList.remove('show4');
+});
+
+
+const open5 = document.getElementById('open5');
+const modal_container5 = document.getElementById('modal_container5');
+const close5 = document.getElementById('close5');
+
+open5.addEventListener('click', () => {
+  modal_container5.classList.add('show5');  
+});
+
+close5.addEventListener('click', () => {
+  modal_container5.classList.remove('show5');
+});
+
+
+
